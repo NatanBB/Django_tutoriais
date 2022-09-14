660dea4bdddb32603d1635f29982ba78bd05cb20
Natan Borba Boos <natanborba77@gmail.com>
2022-09-13 21:51:57 -0300

    Create authenticate user

--- Tutorial_REST_02/config/urls.py
+++ Tutorial_REST_02/config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # config/urls.py
+# config/urls.py
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
